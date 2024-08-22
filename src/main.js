4c9c16a3a1c20e70bab7cd4fd623fbb5e43bed55
QuentinQ <quentin.zt.qian@gmail.com>
2024-08-22 10:55:22 +1000

    update week-4-end

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
-import './assets/main.css'
-// import './style.css'
+// import './assets/main.css'
+// import '@/assets/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-
-const app = createApp(App);
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
-
-import {
-    createApp
-} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+// import DataTable from 'primevue/datatable'
+// import Column from 'primevue/Column'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue, { theme: { preset: Aura } })
+
+// app.component('DataTable', DataTable)
+// app.component('Column', Column)
+
+app.mount('#app')
